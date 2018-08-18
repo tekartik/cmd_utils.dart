b53641ee13a5233a7012b2beceb1cde09c1c994c
Alexandre Roux Tekartik <alex@tekartik.com>
2018-08-18 15:01:31 +0200

    feat: extract kill_cmd

--- bin/kill_cmd.dart
+++ bin/kill_cmd.dart
@@ -1,43 +1,6 @@
-#!/usr/bin/env dart
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+#!/usr/bin/env bash
+library tekartik_script.bin.kill_cmd;
 
-import 'dart:io';
+import 'package:tekartik_cmd_utils/bin/kill_cmd.dart' as _;
 
-import 'package:args/args.dart';
-import 'package:tekartik_cmd_utils/kill_cmd.dart';
-
-const String _FLAG_HELP = 'help';
-const String scriptName = "kill_cmd";
-
-main(List<String> args) async {
-  var parser = new ArgParser();
-
-  parser.addFlag(_FLAG_HELP, abbr: 'h', help: 'Usage help', negatable: false);
-
-  var results = parser.parse(args);
-
-  parser.parse(args);
-
-  bool help = results[_FLAG_HELP];
-
-  _usage() {
-    print(
-        "${scriptName} <part_of_process_name> [<other_part_of_process_name> ...]");
-    print(parser.usage);
-  }
-
-  if (help) {
-    _usage();
-    return;
-  }
-
-  if (results.rest.length == 0) {
-    _usage();
-    exit(1);
-  }
-
-  for (String name in results.rest) {
-    await killCommand(name);
-  }
-}
+main(List<String> arguments) => _.main(arguments);
